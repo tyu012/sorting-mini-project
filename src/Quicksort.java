9f3f95962ca480eb381f8a3845c2e3fccb9f3080
Tim Yu <yuyu.tim@gmail.com>
2024-04-05 21:00:07 -0500

    Moved prior work on Quicksort to src/Quicksort.java

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -3,7 +3,10 @@ import java.util.Comparator;
 /**
  * Sort using Quicksort.
  *
- * @author Your Name Here
+ * @author Tim Yu
+ * @author Zack Abdilahi
+ * 
+ * Based on lab of 4/1 done by myself and Zack Abdilahi.
  */
 
 public class Quicksort implements Sorter {
@@ -35,4 +38,65 @@ public class Quicksort implements Sorter {
   public <T> void sort(T[] values, Comparator<? super T> order) {
     // STUB
   } // sort(T[], Comparator<? super T>
+
+  // +----------------------+----------------------------------------
+  // | Semi-private methods |
+  // +----------------------+
+  
+  /**
+   * Sort the subarray of T given by [lb..ub) in place using
+   * the Quicksort algorithm.
+   */
+  <T> void quicksort(T[] values, Comparator<? super T> order, int lb, int ub) {
+    // STUB
+  } // quicksort(T[], Comparator<? super T>, lb, ub)
+  
+  /**
+   * Select a pivot and partition the subarray from [lb .. ub) into 
+   * the following form.
+   *
+   * <pre>
+   * ---+-----------------+-+----------------+---
+   *    | values <= pivot |p| values > pivot |
+   * ---+-----------------+-+----------------+---
+   *    |                 |                  |
+   *    lb                pivotLoc           ub
+   * </pre>
+   *
+   * @return pivotLoc.
+   */
+  public static <T> int partition(T[] arr, Comparator<? super T> order, int lb, int ub) {
+    int pivotLoc = (ub - lb) / 2;
+    T pivotValue = arr[pivotLoc];
+    int small = lb;
+    int large = ub;
+
+    // swap the pivot with lb
+    swap(arr, small, pivotLoc);
+    small++;
+
+    // compare and swap
+    while (small < large) {
+      if (order.compare(arr[small], pivotValue) <= 0) {
+        small++;
+      } else if (order.compare(arr[large - 1], pivotValue) > 0) {
+        large--;
+      } else {
+        swap(arr, small, large - 1);
+        small++;
+        large--;
+      }
+    }
+
+    return pivotLoc;
+  }
+
+  /**
+   * Swaps the elements at indexA and indexB of given array.
+   */
+  public static <T> void swap(T[] arr, int indexA, int indexB) {
+    T valA = arr[indexA];
+    arr[indexA] = arr[indexB];
+    arr[indexB] = valA;
+  }
 } // class Quicksort
